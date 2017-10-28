182ba2c790ac2c27185b99cf2f39f4b5c8ba829a
RamirezJosue <xdemobtkx@gmail.com>
2017-10-28 16:38:13 -0500

    autorizacion

--- src/conn.js
+++ src/conn.js
@@ -1,6 +1,6 @@
 const config = {
     url: "http://localhost:8003/o/authorize/",
-    client: "GY34PChxF8LLBeZGdNAOLzEv1AThce1U6jFl0t0c",
+    client: "DM4GvwSMqZy3eyB9yOfmPWD33qhzGCWG9scyEBYS",
     redirect: "http://localhost:3000",
     scope: "read",
     width: 400, // Width (in pixels) of login popup window. Optional, default: 400
