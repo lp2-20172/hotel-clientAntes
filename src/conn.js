6bca7ec3363e5f9740e6b9475430a8faf63b7c5c
RamirezJosue <xdemobtkx@gmail.com>
2017-10-28 22:23:44 -0500

    conn URL heroku

--- src/conn.js
+++ src/conn.js
@@ -1,6 +1,7 @@
 const config = {
-    url: "http://localhost:8003/o/authorize/",
-    client: "GukUxTvjXOiC7TY2aM4uZj87ZansZ2DHe6t0a37z",
+    //url: "http://localhost:8003/o/authorize/",
+    url: "https://hotel-serve.herokuapp.com/o/authorize/",
+    client: "Uad1heFvNOiuSqMfVVz1VbIE26WDomaFoALGcDm5",
     redirect: "http://localhost:3000",
     scope: "read",
     width: 400, // Width (in pixels) of login popup window. Optional, default: 400
