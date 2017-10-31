7e5fdc27b6e64a807bd5593f1ada521b0a052255
paulneo <localos2000@gmail.com>
2017-10-31 10:04:06 -0500

    slider rooms

--- src/components/hotel/categorias/Information.js
+++ src/components/hotel/categorias/Information.js
@@ -5,6 +5,14 @@ import {
     Link,
     NavLink
 }from 'react-router-dom'
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
 
 class Information extends Component {
     render() {
@@ -33,4 +41,111 @@ class Information extends Component {
     }
 }
 
-export default Information;
+
+
+const items = [
+  {
+    src: 'http://rossello-barcelona.eveniahotels.com/wp-content/uploads/2013/12/Family.jpg',
+
+    caption:'Cama'
+  },
+  {
+    src: 'http://rossello-barcelona.eveniahotels.com/wp-content/uploads/2013/12/Family.jpg',
+
+    caption: 'Baño'
+  },
+  {
+    src: 'http://rossello-barcelona.eveniahotels.com/wp-content/uploads/2013/12/Family.jpg',
+
+    caption: 'Vista al mar'
+  }
+];
+
+class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          src={item.src}
+        >
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <div >
+         <div className="habitacion habitacion_info" >
+              <div className="img img_info">
+                <Carousel className="asd"
+                  activeIndex={activeIndex}
+                  next={this.next}
+                  previous={this.previous}
+                >
+                  <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                  {slides}
+                  <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                  <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                </Carousel>
+              </div>
+              <div className="info_rooms">
+                  <div className="info info_reserva">
+                     <h3>
+                        Habitacion doble
+                     </h3>
+                     <p>
+                       S/50.soles
+                     </p>
+                     <p>
+                        <NavLink exact to="/forms"  activeClassName="selected">Reservar</NavLink>
+                     </p>
+                  </div>
+             </div>
+          </div>
+      </div>
+
+    );
+  }
+}
+
+export default Example;
+// export default Information;
