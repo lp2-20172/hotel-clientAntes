c89f2891628f6eaca62aa8dadd8c02be0301c1a9
paulneo <localos2000@gmail.com>
2017-11-07 00:36:34 -0500

    add slider

--- src/components/hotel/categorias/List.js
+++ src/components/hotel/categorias/List.js
@@ -29,48 +29,6 @@ const items = [
 ];
 
 
-class List extends Component {
-  componentWillMount() {
-    this.props.getList("")
-  }
-  render() {
-    let { list } = this.props
-    if (list) {
-
-    } else {
-      list = []
-
-    }
-
-    return (
-      <div >
-         { list.map((d, index) =>
-        <div className="habitacion habitacion_info" >
-          <div className="img img_info">
-            <UncontrolledCarousel items={items} />
-          </div>
-          <div className="info_rooms">
-            <div className="info info_reserva">
-              <h3>
-              {d.nombre}
-                     </h3>
-              <p>
-                S/{d.precio}
-                     </p>
-              <p>
-                <NavLink exact to="/forms" activeClassName="selected">Reservar</NavLink>
-              </p>
-            </div>
-          </div>
-          <div className="informance">
-            <p> {d.descripcion}</p>
-          </div>
-        </div>
-      )}
-      </div>
-    );
-  }
-}
 List.propTypes = {
   list: PropTypes.array
 }
