f170ca161cf09518ebd076f46c90a577509e2842
paulneo <localos2000@gmail.com>
2017-11-07 00:35:12 -0500

    ..

--- src/conn.js
+++ src/conn.js
@@ -1,7 +1,7 @@
 const config = {
     url: "http://localhost:8003/o/authorize/",
     //url: "https://hotel-serve.herokuapp.com/o/authorize/",
-    client: "ZHzx5C9XcgphOkmWcs6dqL5YqXtj0DV9znKUZSNQ",
+    client: "CrdL2kq8w0vciXiNuQppfODemJRE1i3esCF8lQTT",
     redirect: "http://localhost:3000",
     scope: "read",
     width: 400, // Width (in pixels) of login popup window. Optional, default: 400
